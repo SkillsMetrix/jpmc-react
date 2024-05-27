d68a76dcf7cacc600dd5c09f9ed5804166c51ebe
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-05-27 12:46:10 +0530

    Update codes.js

--- codes.js
+++ codes.js
@@ -20,3 +20,45 @@ export default class MainApp extends Component {
   }
 }
 
+----------
+
+  import Footer from "./Footer"
+import Header from "./Header"
+import User from "./User"
+
+
+  function Users(props){
+
+    return(
+        <div>
+            
+           {
+            props.ud.map((users) =>  <User usr={users}/> )
+           }
+          
+            
+        </div>
+    )
+  }
+  export default Users
+
+
+
+-------
+
+  
+
+  function User(props){
+
+    return(
+        <div>
+            
+          <ul>
+            <li> {props.usr}</li>
+          </ul>
+          
+           
+        </div>
+    )
+  }
+  export default User
