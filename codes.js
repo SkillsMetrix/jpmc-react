b6377b108e2a1c295568bf4ab2ea76096cbcd155
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-05-27 16:43:04 +0530

    Update codes.js

--- codes.js
+++ codes.js
@@ -1,72 +1,30 @@
-import AddUser from "./AddUser";
-import Footer from "./Footer";
-import Header from "./Header";
-import Users from "./Users";
 import { Component } from "react";
-
-export default class MainApp extends Component {
-
-  //=============Lifecycle-----------
-
-  componentDidMount(){
-   
-    const json=localStorage.getItem('jpmc')
-    const userData= JSON.parse(json)
-    if(userData){
-      this.setState(()=>{
-        return {
-          userData
-        }
-      })
-    }
-
-  }
-  componentDidUpdate(){
-   const json= JSON.stringify(this.state.userData)
-   localStorage.setItem('jpmc',json)
-  }
-  //==== initialize the state=====
+import axios from "axios";
+const URL = "https://jsonplaceholder.typicode.com/users";
+export default class RestApp extends Component {
   state = {
-    headerMessage: "welcome to header",
-    userData: [],
+    users: [],
   };
-  //=====================================Operation===========
-
-  addUser = (data) => {
-    this.setState((prevState) => {
-      return {
-        userData: prevState.userData.concat(data),
-      };
-    });
-  };
-  deleteAll = () => {
-    this.setState(() => {
-      return {
-        userData: [],
-      };
-    });
-  };
-
-  deleteUser=(user)=>{
-     this.setState((prevState)=>{
-      return {
-        userData:prevState.userData.filter((option) => user !== option)
-      }
-     })
+  componentDidMount() {
+    return axios
+      .get(URL)
+      .then((res) => res.data)
+      .then((data) => {
+        console.log(data);
+        this.setState({ users: data });
+      });
   }
+
   render() {
     return (
       <div>
-        <Header hm={this.state.headerMessage} />
-        <p>Welcome to MainApp</p>
-        <Users
-          ud={this.state.userData}
-          da={this.deleteAll}
-          hasData={this.state.userData.length > 0}
-          deleteUser={this.deleteUser}
-        />
-        <AddUser au={this.addUser} />
-        <Footer />
+        <p>User Data</p>
+
+        {this.state.users.map((user) => (
+          <div>
+            {user.name} --- {user.email}
+          </div>
+        ))}
       </div>
     );
   }
