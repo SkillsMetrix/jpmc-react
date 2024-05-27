8d34e398f3e95ee5a8f3394a90738cbd42d334de
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-05-27 12:45:45 +0530

    Update codes.js

--- codes.js
+++ codes.js
@@ -1,47 +1,22 @@
-import AddUser from "./AddUser"
-import Footer from "./Footer"
-import Header from "./Header"
-import Users from "./Users"
+import AddUser from "./AddUser";
+import Footer from "./Footer";
+import Header from "./Header";
+import Users from "./Users";
+import { Component } from 'react'
 
-
-  function MainApp(){
-    const headerMessage='welcome to header'
-    const userData=['admin','manager','qa']
-    return(
-  
-        <div>
-            <Header hm={headerMessage}/>
-            <p>Welcome to MainApp</p>
-            <Users ud={userData}/>
-            <AddUser/>
-            <Footer/>
-        </div>
-    )
+export default class MainApp extends Component {
+  render() {
+    const headerMessage = "welcome to header";
+    const userData = ["admin", "manager", "qa"];
+    return (
+      <div>
+        <Header hm={headerMessage} />
+        <p>Welcome to MainApp</p>
+        <Users ud={userData} />
+        <AddUser />
+        <Footer />
+      </div>
+    );
   }
-  export default MainApp
+}
 
-
-
-import Footer from "./Footer"
-import Header from "./Header"
-
-
-  function AddUser(){
-
-    const addUser=()=>{
-      alert('user is added')
-    }
-    return(
-        <div>
-            <p>User Add</p>
-<form>
-<button onClick={addUser}>Add</button>
-</form>
-  
-            
-   
-           
-        </div>
-    )
-  }
-  export default AddUser
