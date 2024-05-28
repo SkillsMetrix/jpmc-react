49ed773047cb0d637a043636f6595057f36f50ca
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-05-28 10:31:05 +0530

    Update codes.js

--- codes.js
+++ codes.js
@@ -1,30 +1,29 @@
-import React, { useEffect, useState } from "react";
 
-function Demo1(props) {
-  // this is alternative to state={}
-  const [count, setCount] = useState(0);
-  // called during load time
-  useEffect(() => {
-    console.log(" called load time");
-  }, []);
-  //called when state changed
-  useEffect(() => {
-    console.log(" called when state changed");
-  }, [count]);
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+const URL = "https://jsonplaceholder.typicode.com/users";
+export default function RestApp() {
+    // initialize the state
+    const [users,setUsers]= useState([])
 
+    //load the API during onLoad
+    useEffect(()=>{
+        axios.get(URL)
+        .then(res => res.data)
+        .then(data =>{
+            setUsers(data)
+        })
 
-  const inc = () => {
-    setCount(count + 1);
-  };
-
+    },[])
   return (
-    <div>
-      <p>The count is : {count}</p>
-      <hr />
-      <button onClick={inc}>INC</button>
-      <button onClick={() => setCount(count - 1)}>DEC</button>
-    </div>
-  );
-}
+    <div className="container">
+        <p>User Data</p>
 
-export default Demo1;
+        {users.map((user) => (
+          <div>
+            {user.name} --- {user.email}
+          </div>
+        ))}
+      </div>
+  )
+}
