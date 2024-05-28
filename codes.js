56fdc422db8fde362d232f1a2e608fb16e1e5016
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-05-28 11:14:57 +0530

    Update codes.js

--- codes.js
+++ codes.js
@@ -3,17 +3,18 @@ import React, { useState } from "react";
 function UserApp(props) {
   const [users, setUsers] = useState([]);
   const [uname, setUname] = useState("");
+  const [email, setEmail] = useState("");
 
   const addUser = (e) => {
     e.preventDefault();
-    setUsers([...users, { uname }]);
+    setUsers([...users, { uname,email }]);
     setUname("");
   };
   return (
     <div>
       <div>
         {users.map((user) => (
-          <div key={user.uname}>{user.uname}</div>
+          <div key={user.uname}>{user.uname}---{user.email}</div>
         ))}
       </div>
       <hr />
@@ -24,6 +25,13 @@ function UserApp(props) {
           name="uname"
           value={uname}
           onChange={(e) => setUname(e.target.value)}
+        />
+         Email:
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <button>Add User</button>
       </form>
