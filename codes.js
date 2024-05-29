53de999cbe8e69466ec05c84bac19f954366b234
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-05-29 11:26:54 +0530

    Update codes.js

--- codes.js
+++ codes.js
@@ -1,50 +1,21 @@
-const counterReducer = (state = 20, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    default:
-      return state;
-  }
-};
-export default counterReducer;
+import { createSlice } from '@reduxjs/toolkit'
 
+export const counterSlice= createSlice({
 
+    name:'counter',
+    initialState:{
+        counter:10
+    },
 
-----------
+    reducers:{
+        increment:(state)=>{
+            state.counter+=1
+        },
+        decrement:(state)=>{
+            state.counter-=1
+        },
 
-
-  
-
-export const increment=()=>{
-    return{
-        type:"INCREMENT"
     }
-}
-export const decrement=()=>{
-    return{
-        type:"DECREMENT"
-    }
-}
-----
-
-  
-
-import React from 'react';
- import { useDispatch, useSelector } from 'react-redux';
-import { decrement, increment } from '../redux/actions/counterActions';
-function CounterApp(props) {
-    const data= useSelector(state => state.counter)
-    const dispatch=useDispatch()
-    return (
-        <div>
-             The Count is :{data}
-             <hr/>
-             <button onClick={()=> dispatch(increment())}>INC</button>
-             <button onClick={()=> dispatch(decrement())}>DEC</button>
-        </div>
-    );
-}
-
-export default CounterApp;
+})
+export const {increment,decrement}=counterSlice.actions
+export default counterSlice.reducer
