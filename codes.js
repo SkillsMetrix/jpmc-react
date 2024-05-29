e0ad477ba5d5a911d2d791f306d4aa0ddf5b62f6
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-05-29 15:05:52 +0530

    Update codes.js

--- codes.js
+++ codes.js
@@ -1,17 +1,24 @@
-import {call,put,takeEvery} from 'redux-saga/effects'
-import { GET_USER_FETCH, GET_USER_SUCCESS } from './actions'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import createSagaMiddleware from 'redux-saga'
+import appReducer from './redux/reducer';
+import mySaga from './redux/saga';
+import {combineReducers,createStore,applyMiddleware} from 'redux'
+ 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const sm= createSagaMiddleware()
+const rootReducer= combineReducers({appReducer})
+const store= createStore(rootReducer,applyMiddleware(sm))
+sm.run(mySaga)
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
-function usersFetch(){
-    return fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
-}
 
-
-function* workGetUserFetch(){
-    const users=yield call(usersFetch)
-    yield put ({type: GET_USER_SUCCESS},users)
-}
-function* mySaga(){
-    yield takeEvery(GET_USER_FETCH,workGetUserFetch)
-}
-
-export default mySaga
+reportWebVitals();
